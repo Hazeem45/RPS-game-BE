e2598459df727c5df93185f9e33c43d9ae429904
Muhamad Fery Hasyim <hasyim.fery27@gmail.com>
2024-02-21 01:42:53 +0700

    add swagger documentation

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 const userRouter = require("./user/user.route");
 const gameRouter = require("./user-game/game.route");
-const port = 3000;
-
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./documentation/swagger.json");
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/user", userRouter);
 app.use("/game", gameRouter);
 
