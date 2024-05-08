2af10caa0dbcde958cec77f8c9531faca9615ab3
Muhamad Fery Hasyim <hasyim.fery27@gmail.com>
2024-05-08 22:51:25 +0700

    custom encoding base64

--- utils/base64URLSafeCustom.js
+++ utils/base64URLSafeCustom.js
@@ -1,28 +1,39 @@
 const delimiter = "=";
-// Fungsi untuk melakukan URL-safe Base64 Encoding tanpa padding
+// Function to perform URL-safe Base64 Encoding without padding
 const base64EncodeURLSafe = (data) => {
-  return Buffer.from(data + "rpsgames2024/roomId" + delimiter + data, "utf-8")
+  return Buffer.from(data + data + 3 + delimiter + "/roomId?" + delimiter + data, "utf-8")
     .toString("base64")
-    .replace(/\+/g, "-") // Mengganti "+" dengan "-"
-    .replace(/\//g, "_") // Mengganti "/" dengan "_"
-    .replace(/=/g, ""); // Menghapus padding "="
+    .replace(/\+/g, "-") // replace "+" with "-"
+    .replace(/\//g, "_") // replace "/" with "_"
+    .replace(/=/g, ""); // delete padding "="
 };
 
+// Function to decoding
 const base64DecodeURLSafe = (encodedData) => {
-  // Tambahkan padding "=" jika panjang encodedData bukan kelipatan 4
+  // add padding "=" if length encodedData is not a multiple of 4
   while (encodedData.length % 4 !== 0) {
     encodedData += "=";
   }
 
-  // Ganti karakter "_" kembali menjadi "/" dan "-" kembali menjadi "+"
+  // change Character "_" back to "/" and "-" back to "+"
   encodedData = encodedData.replace(/_/g, "/").replace(/-/g, "+");
 
-  // Lakukan Base64 Decoding
+  // Base64 Decoding
   const decodedId = Buffer.from(encodedData, "base64").toString("utf-8");
 
   const parts = decodedId.split(delimiter);
-  const decodedRoomId = parts[1];
-  return decodedRoomId;
+  const trueValue = (parts[0] - 3) / 2;
+  const arrayDelimiter = parts[1];
+  const decodedRoomId = parts[2];
+  if (arrayDelimiter === "/roomId?") {
+    if (trueValue.toString() === decodedRoomId) {
+      return decodedRoomId;
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
 };
 
 module.exports = {base64EncodeURLSafe, base64DecodeURLSafe};
