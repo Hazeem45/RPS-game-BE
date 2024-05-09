c8a2e9c7d9c80a834622a7723da958ddfb844118
Muhamad Fery Hasyim <hasyim.fery27@gmail.com>
2024-05-09 20:18:04 +0700

    fix swagger doc

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./documentation/swagger.json");
 require("dotenv").config();
 
-// CDN CSS
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const options = {
+  customCss: ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+  customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+};
 
 app.use(cors());
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCssUrl: CSS_URL}));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 app.use("/user", userRouter);
 app.use("/game", gameRouter);
 
