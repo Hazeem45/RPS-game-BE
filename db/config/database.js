6f476d967c25a908faa2f7cdf159ed4205434d30
Muhamad Fery Hasyim <hasyim.fery27@gmail.com>
2024-05-12 16:18:21 +0700

    add dialectModules

--- db/config/database.js
+++ db/config/database.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+const pg = require("pg");
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
+    dialectModules: pg,
     dialectOptions: {
       ssl: {
         require: true,
@@ -15,6 +17,7 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
+    dialectModules: pg,
     dialectOptions: {
       ssl: {
         require: true,
